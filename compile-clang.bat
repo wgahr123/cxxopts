5f5a353f6dd611d5852400890207c04f3d9933ba
Wolfgang Gahr <gahrwolfgang@gmail.com>
2019-12-01 15:59:25 +0100

    added -Wall to compiler calls

--- compile-clang.bat
+++ compile-clang.bat
@@ -1,5 +1,5 @@
 wsl clang --version
-wsl clang -Iinclude src/test_desc.cpp -lstdc++
+wsl clang -Wall -Iinclude src/test_desc.cpp -lstdc++
 wsl mv a.out test_desc.clang
 wsl chmod +x test_desc.clang
 wsl ./test_desc.clang -h -w60 -e
--- compile-gcc7.bat
+++ compile-gcc7.bat
@@ -1,5 +1,5 @@
 wsl gcc-7 --version
-wsl gcc-7 -Iinclude src/test_desc.cpp -lstdc++
+wsl gcc-7 -Wall -Iinclude src/test_desc.cpp -lstdc++
 wsl mv a.out test_desc.gcc7
 wsl chmod +x test_desc.gcc7
 wsl ./test_desc.gcc7 -h -w60 -e
